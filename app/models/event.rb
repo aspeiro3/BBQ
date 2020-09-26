4b9273943edb219e5c022d0dc4f91eb7877df10c
aspeiro3@gmail.com <aspeiro3@gmail.com>
2020-09-26 19:15:58 +0200

    events initial

---
+++ app/models/event.rb
@@ -0,0 +1,9 @@
+class Event < ApplicationRecord
+  
+  validates :title,
+            presence: true,
+            length: { maximum: 255 }
+
+  validates :address, presence: true
+  validates :datetime, presence: true
+end
