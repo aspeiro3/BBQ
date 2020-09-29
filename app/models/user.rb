18055962a0f47c68fab365e49297c96c072b577f
aspeiro3@gmail.com <aspeiro3@gmail.com>
2020-09-29 16:33:37 +0200

    fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,15 @@
 class User < ApplicationRecord
+  EMAIL_FORMAT = /\A.+@.+\z/
+
+  has_many :events
+
+  validates :name,
+            presence: true,
+            length: { maximum: 35 }
+
+  validates :email,
+            presence: true,
+            length: { maximum: 255 },
+            uniqueness: true,
+            format: { with: EMAIL_FORMAT }
 end
