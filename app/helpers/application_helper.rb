4adc3f545e23b5031a2c8fc996ebcc34bf8b8f75
aspeiro3@gmail.com <aspeiro3@gmail.com>
2020-10-07 19:48:30 +0200

    2.1 version ready

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -21,6 +21,17 @@ module ApplicationHelper
     return
   end
 
+  def declension_of_words(number, option_one, option_two, option_three)
+    ostatok = number % 10
+    ostatok_11_14 = number % 100
+
+    if ostatok.between?(5, 9) || ostatok == 0 || ostatok.between?(11, 14)
+      return option_three
+    end
+    return option_one if ostatok == 1
+    return option_two if ostatok.between?(2, 4)
+  end
+
   def user_avatar(user)
 #    if user.avatar_url.present?
 #      user.avatar_url
